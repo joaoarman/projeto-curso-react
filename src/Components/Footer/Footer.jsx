cce9ad45b15dbbfea3433beab5e34afcd2b54d76
João Vítor  Arman <joaovarman@gmail.com>
2025-01-16 20:06:16 -0300

    .

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './Footer.module.css' 
+import Dogs from '../../Assets/dogs.svg?react';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      
+      <Dogs />
+        <p>Dogs. Alguns direitos Reservados.</p>
     </footer>
   )
 }
